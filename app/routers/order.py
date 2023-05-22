51569834c872bdc7faf43617beb4d001e118b6e0
ybenannoune <yacine.benannoune@gmail.com>
2023-05-23 00:30:30 +0200

    working on Order schema

--- app/routers/order.py
+++ app/routers/order.py
@@ -6,22 +6,54 @@ from ..database import get_db
 
 router = APIRouter()
 
+# {
+#   "name": "DUPONT",
+#   "phone_number": "010203040506",
+#   "address": "Avenue General Leclerc",
+#   "cartItems": [
+#     {
+#       "dishId": 1,
+#       "quantity": 4,
+#       "modifier": "-piment",
+#       "drink": "Coca"
+#     },
+#     {
+#       "dishId": 1,
+#       "quantity": 2,
+#       "modifier": "-harissa",
+#       "drink": "Fanta"
+#     }
+#   ],
+#   "delivery_date": "2023-05-22T22:25:39.779Z"
+# }
+
 @router.post("/create")
 async def create_order(payload: schemas.CreateOrderSchema, db: Session = Depends(get_db)):
 
     print(payload)
-    
+
     order = models.Order(
         order_date=datetime.utcnow(),
         delivery_date=payload.delivery_date,
-        address=payload.user.address,
-        phone_number=payload.user.tel
+        address=payload.address,
+        phone_number=payload.phone_number
     )
-    
-    # TODO ajouter le contenu de la commande dans la DB    
 
     db.add(order)
     db.commit()
     db.refresh(order)
-    
+
+    for item in payload.cartItems:
+        # dish = db.query(models.Dish).get(item.dishId)
+        order_item = models.OrderItems(
+            order_id=order.id,
+            dish_id=item.dishId,
+            quantity=item.quantity,
+            modifiers=item.modifier,
+            drink=item.drink
+        )
+        db.add(order_item)
+
+    db.commit()
+
     return {"order_id": order.id}
